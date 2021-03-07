8f2f7215afbdd2aa0b78dbe8cb1e647018be9b0a
yvhime <yvhime@gmail.com>
2021-03-07 22:41:14 +0800

    this keyword

--- sandbox.js
+++ sandbox.js
@@ -9,20 +9,39 @@ people.forEach(function(person) {
 console.log(html);
 ul.innerHTML = html; // gets html to be placed on ul using innerHTML
 
+// const blogsArray = [
+//     { title: "why mac & cheese rules", likes: 30},
+//     { title: "10 things to make with marmite", likes: 50}
+// ]; // array blogs with objects inside
+
+// console.log("array blogs " + blogsArray);
+
 let user = {
     name: "crystal", // key:value pair == 1 property
     age: 30,
     email: "crystal@thenetninja.co.uk",
     location: "berlin",
-    blogs: ["why mac & cheese rules", "10 things to make with marmite"],
+    // blogs: ["why mac & cheese rules", "10 things to make with marmite"],
+    blogs: [
+        { title: "why mac & cheese rules", likes: 30},
+        { title: "10 things to make with marmite", likes: 50}
+    ],
     login: function() { // login is the name of the method // still key:value pair
         console.log("the user logged in");
     },
     logout: function() {
         console.log("the user logged out");
     },
+    // logout() {
+    //     console.log("the user logged out");
+    // }, is the same as on the op
     logBlogs: function() {
-        
+        // this.blogs; // refers to user? // represents the user object
+        // console.log("this keyword " + this.blogs);
+        console.log("this user has written the following blogs: ");
+        this.blogs.forEach(function(blog) { // callback function // blog parameter represents the object inside blogs key
+            console.log(blog.title + " " + blog.likes + " " + "ARROW FUNC ON NETNINJA");
+        }) // this.blogs is an array forEach is a method // 
     }
 };
 
@@ -33,3 +52,5 @@ console.log(user.name); // dot notation to check specific data
 console.log(user['name']); // using brackets
 user.login();
 user.logout();
+user.logBlogs();
+console.log(this); // global context global object window object
